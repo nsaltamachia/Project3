c86fb72757dd9591dab9a832dc5fb38548749b73
K Castillo <krystalin.castillo@gmail.com>
2023-11-22 16:30:17 -0600

    Super users added

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+# import user for auth
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Restaurant(models.Model):
+    # foreign key linking user to instance (auth)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
